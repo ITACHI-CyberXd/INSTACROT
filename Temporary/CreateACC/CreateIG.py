f31dbac1d8b3d7bfc008354f6d3f08919f677f53
ArifSensei-Smrg <ariffmonsep@gmail.com>
2024-12-17 23:19:22 +0700

    Add files via upload

---
+++ Temporary/CreateACC/CreateIG.py
@@ -0,0 +1,11 @@
+import os, sys
+
+class CreateIG:
+    def __init__(self) -> None:
+        self.TahapPengembang()
+        pass
+        
+    def TahapPengembang(self):
+        try: os.system('clear'); sys.exit('[#] MENU BERADA DI FOLDER AWAL Runya python CreateIG.py')
+        except (Exception) as e: pass
+        
